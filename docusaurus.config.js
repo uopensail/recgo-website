d6b1814dd3f0510370e18afc18e7258cc41988df
kungjim <7629149+kungjim@users.noreply.github.com>
2025-02-19 07:37:44 +0800

    update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -110,7 +110,7 @@ const config = {
             items: [
               {
                 label: 'Tutorial',
-                to: '/docs/user documentation/introduction',
+                to: '/docs/user-documentation/introduction',
               },
             ],
           },
